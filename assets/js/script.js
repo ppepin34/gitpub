f5f8893526acf4d8998e8f47a6ac69f0935f41b0
Phil Pepin <ppepin34@gmail.com>
2022-04-10 14:18:00 -0400

    initial search by city and state working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-var listRichmond
-
-var listStuff = function() {
-    console.log("check")
-    var apiURL = "https://api.openbrewerydb.org/breweries?by_city=richmond"
-    fetch(apiURL).then(function(response){
+var listStuff = function () {
+    var apiURL = "https://api.openbrewerydb.org/breweries?by_city=richmond&by_state=virginia"
+    fetch(apiURL).then(function (response) {
         if (response.ok) {
-            response.json().then (function (data) {
+            response.json().then(function (data) {
                 console.log(data)
             });
         } else {
@@ -14,5 +11,45 @@ var listStuff = function() {
     })
 }
 
+// search for breweries in a city and state
+$("#location-search").submit(function (event) {
 
-listStuff();
+    // prevent refresh
+    event.preventDefault();
+
+    // get info from form
+
+    var city = ($("#city").val());
+
+    var state = ($("#state").val());
+
+    // convert city to proper format
+    city = city.replace(" ", "_");
+    city = city.toLowerCase();
+
+    // generate api url
+    var apiURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state;
+
+    // make request
+    fetch(apiURL).then(function (response) {
+        // request was successful
+        if (response.ok) {
+            response.json().then(function (data) {
+
+                // if data = null, return error
+                if (data.length === 0) {
+                    console.log("No breweries found")
+                }
+                // else log data
+                else {
+                    console.log(data)
+                }
+                });    
+        }
+    })
+
+    // response fails
+    .catch(function (error) {
+        alert("Unable to connect to the database");
+    });
+})
