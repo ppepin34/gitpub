b1a7e35e2a79673024b7001335abb7097829f332
Phil Pepin <ppepin34@gmail.com>
2022-04-10 12:54:30 -0400

    intitial html and CSS outline

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    colors: {
+      'dark-gray': '#242331',
+      'coffee': '#533e2d',
+      'golden-brown': '#a27035',
+      'camel': '#b88b4a',
+      'flax': '#ddca7d'
+    },
     extend: {},
   },
   plugins: [],
